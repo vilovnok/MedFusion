3b03ea41e61eb1f34569237c3b7427313e361742
vilovnok <vilovnok@gmail.com>
2024-12-07 21:46:22 +0300

    fix

--- README.md
+++ README.md
@@ -9,22 +9,12 @@ poetry shell
 
 ## Run all microservers
 ```bash
-docker compose up -d rabbitmq redis qdrant
+docker compose up -d qdrant
 ```
 
 ## Run backend
-```bash
-chmod +x script_name.sh
-```
 Make sure that the agent is working correctly.
-```bash
-./run.sh agent
-```
 FastAPI
 ```bash
 ./run.sh fastapi
-```
-Celery
-```bash
-./run.sh celery
 ```
--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if [ "$1" == "celery" ]; then
-    poetry run celery --app=backend.worker.tasks worker --loglevel=info
+if [ "$1" == "angular" ]; then
+    cd frontend
+    ng serve
 elif [ "$1" == "fastapi" ]; then
     poetry run uvicorn backend.main:app --reload --host 0.0.0.0 --port 8000
-elif [ "$1" == "agent" ]; then
-    poetry run python agent/main.py
 fi
