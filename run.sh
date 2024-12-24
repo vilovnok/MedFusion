292f16513d03b642ffe320edf3b6dd824297b858
vilovnok <vilovnok@gmail.com>
2024-12-25 00:06:15 +0300

    on click

--- run.sh
+++ run.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-if [ "$1" == "angular" ]; then
-    cd frontend
-    ng serve
-elif [ "$1" == "fastapi" ]; then
-    poetry run uvicorn backend.main:app --reload --host 0.0.0.0 --port 8000
+echo "Setup Docker Compose..."
+docker-compose up -d
+
+if [ $? -ne 0 ]; then
+    echo "Ошибка при запуске Docker Compose. Проверьте конфигурацию docker-compose.yml."
+    exit 1
+fi
+
+echo "Install poetry"
+pip install poetry
+
+echo "Setup libs"
+poetry install
+poetry shell
+
+echo "Insert data to PostgreSQL..."
+poetry run python -m backend.src.migration.main --action create
+
+echo "Setup Agent..."
+poetry run python -m backend.src.main
+
+
+if [ $? -ne 0 ]; then
+    echo "Ошибка при запуске FastAPI. Проверьте код вашего приложения."
+    exit 1
 fi
